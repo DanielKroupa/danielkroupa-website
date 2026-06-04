8cd4001abdef6f45be7a30143a3596f0b55f324c
Daniel Kroupa <d_kroupa@utb.cz>
2026-06-04 21:19:56 +0200

    Improved redirect  handling

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,12 @@ import {
   defaultStreamHandler,
 } from "@tanstack/react-start/server";
 
-export const fetch = createStartHandler(defaultStreamHandler);
+export const fetch = createStartHandler((event) => {
+  const response = defaultStreamHandler(event);
+
+  if (!response) {
+    return new Response("Not Found", { status: 404 });
+  }
+
+  return response;
+});
