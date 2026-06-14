95ebcb7d6cd2318927e0502137849090a6c6d7b1
Daniel Kroupa <d_kroupa@utb.cz>
2026-06-14 19:01:12 +0200

    improved build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,22 @@ import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   resolve: {
     tsconfigPaths: true,
   },
